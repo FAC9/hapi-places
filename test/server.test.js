1a0563bd53b4395ccba91d9fcdf983baccb182f7
John Whiles <johnwhiles@outlook.com>
2016-11-22 15:31:47 +0000

    add tests for server

--- test/server.test.js
+++ test/server.test.js
@@ -10,3 +10,14 @@ tape('Server should start', (t) => {
     });
   });
 });
+
+tape('Test server using injection', (t) => {
+  server.inject('/', (res) => {
+    t.ok(res.statusCode === 200, 'homepage request should return statuscode 200');
+    t.ok(res.payload.indexOf('background: linear-gradient') > -1, 'This should return the homepage');
+  });
+  server.inject('letsdancebydavidbowie', (res) => {
+    t.ok(res.statusCode >= 400, 'should error with bad request');
+    t.end();
+  });
+});
