a180f276fccd26af6baaf0b1b5525a5dd2796133
Nick Field <heyimnick@live.com>
2016-11-22 15:57:09 +0000

    added notes on function needed

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,29 @@
+const Req = require('./request');
+const cityDataBuilder = require('./citydatabuilder');
 
+// define routes -
+// /cost-cheaper   (inside handler function, we want to pass uri for cheapest results call to general request function)
+// /temp-colder
+
+// /temp-warmer
+// /internet-faster
+// /air-better
+// /coffee-cheaper
 module.exports = [
 
   {
     method: 'GET',
     path: '/',
     handler: (req, rep) => {
-  //    let schema getdata()
+      // var schema = cityDataBuilder();
+      // There should be function here which uses the following sequence;
+      // // 1. Randomly pick citylist uri
+      // // 2. Generate Nomadlist API call with that uri
+      // // 3. When receiving data, retrieve appropriate values and put them in schema object
+      // // 4. Take city value, then make wikipedia api call
+      // // 5. Retrieve wikipedia data, then parse response data, store in schema object as description value
+      // // 6. also parse in wikipedia link
+      // results object is then injected into view
       const schema = {
         city: 'MADRID',
         cost: '$1000',
@@ -13,7 +31,8 @@ module.exports = [
         internet: 'good',
         air: '78%',
         coffee: '$2',
-        description: 'Madrid, what a town. A hell of a town. A town that never slows down ;)'
+        description: 'Madrid, what a town. A hell of a town. A town that never slows down ;)',
+        wiki: 'https://wikipedia.org'
       };
       rep.view('index', schema);
     }
