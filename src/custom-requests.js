1e283f90bea13499f27060163433812e4b7fd549
John Whiles <johnwhiles@outlook.com>
2016-11-22 17:59:43 +0000

    remove old comments

--- src/custom-requests.js
+++ src/custom-requests.js
@@ -14,14 +14,3 @@ function makeUrl (type, val) {
 }
 
 module.exports = makeUrl;
-
-// `https://nomadlist.com/api/v2/filter/city?c=1&f1_target=short_term_cost_in_usd&f1_type=lt&f1_max=${cost}`
-//
-// `https://nomadlist.com/api/v2/filter/city?c=1&f1_target=temperatureC&f1_type=gt&f1_min=${temp}`;
-// `https://nomadlist.com/api/v2/filter/city?c=1&f1_target=temperatureC&f1_type=lt&f1_max=${temp}`;
-//
-// `https://nomadlist.com/api/v2/filter/city?c=1&f1_target=internet_speed&f1_type=gt&f1_min=${internet}`;
-//
-// `https://nomadlist.com/api/v2/filter/city?c=1&f1_target=safety_level&f1_type=gt&f1_min=${safety}`;
-//
-// `https://nomadlist.com/api/v2/filter/city?c=1&f1_target=coffee_in_cafe&f1_type=lt&f1_max=${maxCoffee}`
