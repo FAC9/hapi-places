43e920ba800d49e8ec020d207fc9141dcb8e6eab
denesnori <denes.nori@yahoo.com>
2016-11-23 10:50:13 +0000

    fix large number display and search

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.table__button').forEach((button) => {
   let requestType = button.name;
-  let requestValue = (button.parentElement.previousElementSibling.textContent).match(/\d+(\.\d+){0,1}/
-)[0]  ;
+  let requestValue = (button.parentElement.previousElementSibling.textContent).match(/\d+(\.?,?\d+){0,1}/
+)[0].replace(',', '');
   let requestURL = `/request?type=${requestType}&value=${requestValue}`;
   button.href = requestURL;
 });
