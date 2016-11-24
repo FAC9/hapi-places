d7cfe1e3b998c8f5a5d3c17563ea2e49a603b817
John Whiles <johnwhiles@outlook.com>
2016-11-24 11:33:58 +0000

    split routes into individual objects, export an array of these objects

--- src/routes.js
+++ src/routes.js
@@ -1,43 +1,49 @@
 const cityDataBuilder = require('./citydatabuilder');
 const customUrlBuilder = require('./custom-requests.js');
-module.exports = [
 
-  {
-    method: 'GET',
-    path: '/',
-    handler: (req, rep) => {
-      cityDataBuilder('', (err, schema) => {
-        if (err) {
-          throw err;
-        } else {
-          rep.view('index', schema);
-        }
-      });
-    }
-  },
-  {
-    method: 'GET',
-    path: '/request',
-    handler: (req, rep) => {
-      let qType = req.query.type;
-      let qValue = req.query.value;
-      let nomadUrl = customUrlBuilder(qType, qValue);
-      cityDataBuilder(nomadUrl, (err, schema) => {
-        if (err) {
-          throw err;
-        } else {
-          rep.view('index', schema);
-        }
-      });
-    }
-  },
-  {
-    method: 'GET',
-    path: '/{path*}',
-    handler: {
-      directory: {
-        path: '.'
+const home = {
+  method: 'GET',
+  path: '/',
+  handler: (req, rep) => {
+    cityDataBuilder('', (err, schema) => {
+      if (err) {
+        throw err;
+      } else {
+        rep.view('index', schema);
       }
+    });
+  }
+};
+
+const request = {
+  method: 'GET',
+  path: '/request',
+  handler: (req, rep) => {
+    let qType = req.query.type;
+    let qValue = req.query.value;
+    let nomadUrl = customUrlBuilder(qType, qValue);
+    cityDataBuilder(nomadUrl, (err, schema) => {
+      if (err) {
+        throw err;
+      } else {
+        rep.view('index', schema);
+      }
+    });
+  }
+};
+
+const files = {
+  method: 'GET',
+  path: '/{path*}',
+  handler: {
+    directory: {
+      path: '.'
     }
   }
+};
+
+module.exports = [
+  home,
+  request,
+  files
 ];
