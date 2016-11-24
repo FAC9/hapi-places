009dd8b4c678ba3a489b26b120e78cdc5e854b6e
Nick Field <heyimnick@live.com>
2016-11-24 10:43:04 +0000

    added wikiRequest functionality to ensure a prompt to visit wikipedia is given, if no appropriate article found. Tested everything, all good

--- src/cities/wiki.js
+++ src/cities/wiki.js
@@ -1,6 +1,11 @@
 function parseWiki (city) {
-  if (city.indexOf('is a magical place') > -1) return city;
-  if (typeof city !== 'object') city = JSON.parse(city);
+  if (typeof city === 'string') {
+    if (city.indexOf('is a city that resides in') > -1) {
+      return city;
+    } else {
+      city = JSON.parse(city);
+    }
+  }
   var html = Object.keys(city.query.pages).map((item) => { return city.query.pages[item]; })[0].extract;
   // var html = Object.values(city.query.pages)[0].extract;
   html = html.replace(/<(.|\n)*?>/g, '');
--- src/request.js
+++ src/request.js
@@ -43,12 +43,13 @@ function cityImageCheck (currentImgUrl, country, cb) {
   });
 }
 
-function wikiRequest (city, cb) {
+function wikiRequest (city, country, cb) {
   Req(wikiUrlBuilder(city), (err, res, body) => {
+    let cityCheckRegEx = /city|town|capital|population|country|climate/ig;
     if (err) {
       cb(err);
-    } else if (JSON.parse(body).query.pages[-1]) {
-      cb(null, `${city} is a magical place...`);
+    } else if (JSON.parse(body).query.pages[-1] || body.split(cityCheckRegEx).length < 2) {
+      cb(null, `${city} is a city in ${country}. Click here to find about more on Wikipedia.`);
     } else {
       cb(null, body);
     }
